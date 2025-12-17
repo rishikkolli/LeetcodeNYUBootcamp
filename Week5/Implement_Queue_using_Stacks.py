d16491b4ebbc15d99997b775d069ec78d8cd297e
rishikkolli <52936833+rishikkolli@users.noreply.github.com>
2025-12-17 09:21:13 -0700

    Clean up MyQueue implementation by removing comments
    Removed comments and example usage from MyQueue class.

---
+++ Week5/Implement_Queue_using_Stacks.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        self._move_in_to_out()
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        self._move_in_to_out()
+        return self.out_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.in_stack and not self.out_stack
+
+    def _move_in_to_out(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+
