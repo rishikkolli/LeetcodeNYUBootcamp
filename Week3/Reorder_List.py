8fb57d2c997846a48222941f8dc744ee69c263d6
rishikkolli <52936833+rishikkolli@users.noreply.github.com>
2025-12-17 12:34:03 -0700

    Add reorderList method to Solution class
    Implement reorderList method to reorder linked list.

--- Week3/Reorder_List.py
+++ Week3/Reorder_List.py
@@ -1,1 +1,24 @@
-hi
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+        
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev, curr = None, slow.next
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        slow.next = None 
+        
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
